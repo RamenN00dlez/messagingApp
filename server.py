acdcbc449e03effaf85711512730d744187b5e1e
hackaman <hackaman@pop-os.localdomain>
2021-01-31 16:22:40 -0700

    added regex validity checks on IPs and Portnums for users and contact list memebers

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
 import sys
+from datetime import datetime
+import time
+import re
 
+#regular expression to match an IPv4 address
+IPv4 = "^(2[0-5][0-5]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(2[0-5][0-5]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(2[0-5][0-5]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(2[0-5][0-5]|1[0-9][0-9]|[1-9][0-9]|[0-9])$"
+#Regular expression to match port number in the range 1024-65353
+Port = "^6535[0-3]|653[0-4][0-9]|65[0-2][0-9][0-9]|6[0-4][0-9][0-9][0-9]|5[0-9][0-9][0-9][0-9]|4[0-9][0-9][0-9][0-9]|3[0-9][0-9][0-9][0-9]|2[0-9][0-9][0-9][0-9]|1[0-9][0-9][0-9][0-9]|[2-9][0-9][0-9][0-9]|102[4-9]|10[3-9][0-9]|1[1-9][0-9][0-9]$"
 class List:
     def __init__(self, arr):
         self.name = arr[0]
@@ -32,11 +39,12 @@ def print_config():
         print("\tNew List: " + contact_lists[l].name+ "," + contact_lists[l].count)
         for k in range(int(contact_lists[l].count)):
             print("\t\t" + contact_lists[l].members[k].name + "," + contact_lists[l].members[k].ip + "," + contact_lists[l].members[k].port)
+    print("\n")
     
 
 #save the current config file. return with appropriate response code
 def save(filename):
-    print("saving configuration to file: " + filename + "...")
+    print("[" + datetime.now().strftime("%H:%M:%S") + "] Saving configuration to file: " + filename + "...")
     try:
         f = open(filename, "w")
         global usercount, contact_names, listcount, contact_lists
@@ -49,27 +57,64 @@ def save(filename):
             for k in range(int(contact_lists[l].count)):
                 f.write(contact_lists[l].members[k].name + "," + contact_lists[l].members[k].ip + "," + contact_lists[l].members[k].port + "\n")
         f.close()
+        print("[" + datetime.now().strftime("%H:%M:%S") + "] Configuration file " + filename + " successfully saved.")
         return True
     except:
+        print("[" + datetime.now().strftime("%H:%M:%S") + "] Configuration save failed.")
         return False
 
 #load a passed config file into the contacts. Only meant for startup
 def load(filename):
-    f = open(filename, "r")
-    global usercount, contact_names, listcount, contact_lists
-    usercount = f.readline().strip()
-    for i in range(int(usercount)):
-        person = Person(f.readline().strip().split(","))
-        contact_names.append(person)
-    listcount = f.readline().strip()
-    for l in range(int(listcount)):
-        lst = List(f.readline().strip().split(","))
-        contact_lists.append(lst)
-        for k in range(int(contact_lists[l].count)):
-            contact = Person(f.readline().strip().split(","))
-            contact_lists[l].members.append(contact)
-    f.close()
+    try:
+        print("[" + datetime.now().strftime("%H:%M:%S") + "] Loading configuration from file: " + filename + "...")
+        f = open(filename, "r")
+        global usercount, contact_names, listcount, contact_lists
+        usercount = f.readline().strip()
+        #add each user to the address book
+        for i in range(int(usercount)):
+            person = Person(f.readline().strip().split(","))
+            #If a user's IP is not a valid address, fail.
+            if(re.match(IPv4, person.ip) == None or re.match(Port, person.port) == None):
+                print("[" + datetime.now().strftime("%H:%M:%S") + "] Configuration load failed.")
+                usercount = "0"
+                listcount = "0"
+                contact_names = []
+                contact_lists = []
+                return False
+            contact_names.append(person)
+        listcount = f.readline().strip()
+        #add each contact list
+        for l in range(int(listcount)):
+            lst = List(f.readline().strip().split(","))
+            contact_lists.append(lst)
+            #add each user in the contact list to the respective list
+            for k in range(int(contact_lists[l].count)):
+                contact = Person(f.readline().strip().split(","))
+                #If a user's IP is not a valid address, fail.
+                if(re.match(IPv4, contact.ip) == None or re.match(Port, contact.port) == None):
+                    print("[" + datetime.now().strftime("%H:%M:%S") + "] Configuration load failed.")
+                    usercount = "0"
+                    listcount = "0"
+                    contact_names = []
+                    contact_lists = []
+                    return False
+                contact_lists[l].members.append(contact)
+        f.close()
+        print("[" + datetime.now().strftime("%H:%M:%S") + "] Configuration file: " + filename + " successfully loaded.")
+        return True
+    except:
+        print("[" + datetime.now().strftime("%H:%M:%S") + "] Configuration load failed.")
+        contact_names = []
+        contact_lists = []
+        return False
 
+#remove a contact name from the active users list and any contact-list they are in
+def exit(name):
+    try:
+        
+        return True
+    except:
+        return False
 #start the program here
 def main():
     #store the specified port number, if none was given, alert the user and exit the program
