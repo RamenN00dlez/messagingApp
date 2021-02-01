4f93ed12351736029e13e1f1c771832f276ef98b
Derek Ratliff <dmratlif@asu.edu>
2021-02-01 02:49:07 -0700

    networked client and server implemented.

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import re
 from socket import *
 
 #regular expression to match an IPv4 address
@@ -15,16 +16,49 @@ username = ""
 #Verify whether or not a command is valid. True if yes, False if no
 def verify_input(cmd):
     cmd = cmd.split(" ")
-    
+    cmdc = len(cmd)
+    if(cmd[0] == "register" and cmdc == 4):
+        if(re.match(IPv4, cmd[2]) != None and re.match(Port, cmd[3])):
+            return True
+    elif(cmd[0] == "create" and cmdc == 2):
+        return True
+    elif(cmd[0] == "query-lists" and cmdc == 1):
+        return True
+    elif(cmd[0] == "join" and cmdc == 3):
+        return True
+    elif(cmd[0] == "leave" and cmdc == 3):
+        return True
+    elif(cmd[0] == "exit" and cmdc == 2):
+        return True
+    elif(cmd[0] == "im-start" and cmdc == 3):
+        return True
+    elif(cmd[0] == "im-complete" and cmdc == 3):
+        return True
+    elif(cmd[0] == "save" and cmdc == 2):
+        return True
+    elif(cmd[0] == "help" and cmdc == 1):
+        print("\tregister <contact-name> <IP-address> <port>\n\tcreate <contact-list-name>\n\tquery-lists\n\tjoin <contact-list-name> <contact-name>\n\tleave <contact-list-name> <contact-name>\n\texit <contact-name>\n\tim-start <contact-list-name> <contact-name>\n\tim-complete <contact-list-name> <contact-name>\n\tsave <file-name>")
     return False
     
 def main():
     if(len(sys.argv) != 3):
         print("\033[91m[ERROR]\033[0m Must define server ip and port number!\n\t\tFormat: ./client.py <server ip> <server port>")
         sys.exit()
+    
+    response = ""
     serverip = sys.argv[1]
     serverport = int(sys.argv[2])
-    
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    while(True):
+        msg = input("Please enter command.\n> \033[994m")
+        if(verify_input(msg)):
+            clientSocket.sendto(msg.encode(), (serverip, serverport))
+            response, addr = clientSocket.recvfrom(2048)
+            print(response.decode())
+            if(msg.split(" ")[0] == "exit"):
+                sys.exit()
+        else:
+            print("Invalid syntax. Command not sent.")
 
 
 if(__name__ == '__main__'):
--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 import sys
 from datetime import datetime
-import time
+from socket import *
 import re
 
 #regular expression to match an IPv4 address
@@ -61,10 +61,10 @@ def save(filename):
                 f.write(contact_lists[l].members[k].name + "," + contact_lists[l].members[k].ip + "," + contact_lists[l].members[k].port + "\n")
         f.close()
         dialog("Configuration sucessfully saved to file \033[1m" + filename + "\033[0m.")
-        return True
+        return "SUCCESS"
     except:
         dialog("Configuration save failed.")
-        return False
+        return "FAILURE"
 
 #load a passed config file into the contacts. Only meant for startup
 def load(filename):
@@ -83,7 +83,7 @@ def load(filename):
                 listcount = "0"
                 contact_names = []
                 contact_lists = []
-                return False
+                return "FAILURE"
             contact_names.append(person)
         listcount = f.readline().strip()
         #add each contact list
@@ -100,37 +100,37 @@ def load(filename):
                     listcount = "0"
                     contact_names = []
                     contact_lists = []
-                    return False
+                    return "FAILURE"
                 contact_lists[l].members.append(contact)
         f.close()
         dialog("Successfully loaded configuration file \033[1m" + filename + "\033[0m.")
-        return True
+        return "SUCCESS"
     except:
         dialog("Failed to load configuration file \033[1m" + filename + "\033[0m.")
         usercount = "0"
         listcount = "0"
         contact_names = []
         contact_lists = []
-        return False
+        return "FAILURE"
 
 #add a user to the contact list. Failure if the user already exists
 def register(name, ip, port):
     dialog("Attempting to add user with details:\n\t\tname = \033[1m" + name + "\033[0m\n\t\tip   = \033[1m" + ip + "\033[0m\n\t\tport = \033[1m" + port + "\033[0m\n")
     if(re.match(IPv4, ip) == None or re.match(Port, port) == None):
         dialog("Failed to add user \033[1m" + name + "\033[0m.")
-        return False
+        return "FAILURE"
     global usercount, contact_names
     for i in range(int(usercount)):
         #check if the username is taken or not.
         if(contact_names[i].name == name or (contact_names[i].ip == ip and contact_names[i].port == port)):
             dialog("Failed to add user \033[1m" + name + "\033[0m.")
-            return False
+            return "FAILURE"
     #the user is not present, add them to the list.
     contact = Person([name, ip, port])
     contact_names.append(contact)
     usercount = str(int(usercount) + 1)
     print_config()
-    return True
+    return "SUCCESS"
 
 #create a new contact list, return failure if the list already exists or number of maximum lists exeeded
 def create(list_name):
@@ -140,12 +140,12 @@ def create(list_name):
     for i in range(int(listcount)):
         if(contact_lists[i].name == list_name):
             dialog("Failed to create contact list.")
-            return False
+            return "FAILURE"
     lst = List([list_name, "0"])
     contact_lists.append(lst)
     listcount = str(int(listcount) + 1)
     dialog("Contact list \033[1m" + list_name + "\033[0m successfully created.")
-    return True
+    return "SUCCESS"
 
 #remove a contact name from the active users list and any contact-list they are in
 #TODO: check if user is in an ongoing IM. return failure if they are.
@@ -169,42 +169,59 @@ def exit(name):
                     contact_lists[l].count = str(int(contact_lists[l].count)-1)
                     break
         dialog("User \033[1m" + name + "\033[0m was successfully removed.")
-        return True
+        return "SUCCESS"
     except:
         usercount = tmpusrct
         listcount = tmplstct
         dialog("Failed to remove user \033[1m" + name + "\033[0m.")
-        return False
+        return "FAILURE"
 
 
 #start the program here
 def main():
     #store the specified port number, if none was given, alert the user and exit the program
     if(len(sys.argv) > 1):
-        serverPort = sys.argv[1]
-        if(re.match(Port, serverPort) == None):
-            print("ERROR: Port number must be in the range 1024-65353. Additional restrictions to port range may apply to usage.")
+        serverPort = int(sys.argv[1])
+        if(re.match(Port, str(serverPort)) == None):
+            print("\033[91m[ERROR]\033[0m: Port number must be in the range 1024-65353. Additional restrictions to port range may apply to usage.")
             sys.exit()
     else:
         print("ERROR: Port number must be provided\n[./server.py <port number> (contact list)]\nport number necessary, contact list file is optional.\n")
         sys.exit()
 
-    print("Starting IM Server process on port #" + serverPort + "...")
+    print("Starting IM Server process on port #" + str(serverPort) + "...")
     #if a contact list file was given, load it.
     if(len(sys.argv) > 2):
         print("Loading contacts from list: " + sys.argv[2] + "...")
         load(sys.argv[2])
-    print_config()
-    save("before")
-    exit("gabe")
-    save("after")
-    register("noob_master69", "192.168.192.255", "60000")
-    create("newlist")
-    print_config()
-    create("otherlist")
-    print_config()
-    save("edgetest")
 
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', serverPort))
+    dialog("\033[92mServer ready to receive...\033[0m")
+    while(True):
+        recv, clientAddr = serverSocket.recvfrom(2048)
+        dialog("Message received from: \033[1m" + clientAddr[0] + "\033[0m\n\t\t" + recv.decode())
+        response = ""
+        cmd = recv.decode().split(" ")
+        if(cmd[0] ==  "register"):
+            response = register(cmd[1], cmd[2], cmd[3])
+        elif(cmd[0] == "create"):
+            response = create(cmd[1])
+        elif(cmd[0] ==  "query-lists"):
+            response = query_lists()
+        elif(cmd[0] == "join"):
+            response = join(cmd[1], cmd[2])
+        elif(cmd[0] ==  "leave"):
+            response = leave(cmd[1], cmd[2])
+        elif(cmd[0] == "exit"):
+            response = exit(cmd[1])
+        elif(cmd[0] ==  "im-start"):
+            response = im_start(cmd[1], cmd[2])
+        elif(cmd[0] == "im-complete"):
+            response = im_complete(cmd[1], cmd[2])
+        elif(cmd[0] ==  "save"):
+            response = save(cmd[1])
+        serverSocket.sendto(response.encode(), clientAddr)
 
 if(__name__ == '__main__'):
     main()
